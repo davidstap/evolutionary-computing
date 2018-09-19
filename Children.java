bc7f8256311a59a13950e6fe5c048f60f6171784
Tychokoster <tychokoster@hotmail.com>
2018-09-19 15:36:22 +0200

    Children class

---
+++ Children.java
@@ -0,0 +1,21 @@
+public class Children
+{
+    private double[] child1;
+    private double[] child2;
+
+    public Children(double[] child1, double[] child2)
+    {
+        this.child1 = child1;
+        this.child2 = child2;
+    }
+
+    public double[] getChild1()
+    {
+    	return child1;
+    }
+
+	public double[] getChild2()
+    {
+    	return child2;
+    }
+}
